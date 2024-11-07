2c7f1ad7529426566c6ba2da526ce28e5fa290db
peternmuller <pm563838@gmail.com>
2024-11-07 19:32:02 +0100

    alioth: Introduce XiaomiParts * Checkout displayfeature color service, High touch polling rate, Lineage FileUtils and thermal service from https://github.com/pa-gr/android_device_xiaomi_sm8450-common/commits/vauxite/parts
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.sf.native_mode=269 \
@@ -256,6 +260,7 @@ PRODUCT_PACKAGES += \
     AliothSettings \
     AliothSystemUI \
     AOSPAAliothFrameworks \
+    AOSPAAliothSettingsOverlay \
     AOSPAAliothSystemUI \
     SettingsOverlayM2012K11AG \
     SettingsOverlayM2012K11AI \
